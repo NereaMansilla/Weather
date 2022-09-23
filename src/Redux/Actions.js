58a62e6bd40895d44dc97ab2b16f89b9727fc0ae
NereaMansilla <evalbarreiro@hotmail.com>
2022-09-23 16:33:39 -0300

    changes .env again

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -11,7 +11,7 @@ export function getCity(city) {
    return async function (dispatch) {
       try {
          dispatch({ type: LOADING, payload: true })
-         const ciudad = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_KEY }&q=${city}&days=5&aqi=no&alerts=no`)
+         const ciudad = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=ee7ed3f16f36487e80f62924220607&q=${city}&days=5&aqi=no&alerts=no`)
          dispatch({ type: LOADING, payload: false })
          return dispatch({
             type: GET_CITY,
