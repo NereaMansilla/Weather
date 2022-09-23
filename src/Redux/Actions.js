8fd9b9cdf135c73ad9f5a1fd3b1cd039f42a1584
NereaMansilla <evalbarreiro@hotmail.com>
2022-09-23 04:18:59 -0300

    changes

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -10,18 +10,20 @@ export const DARK_MODE = 'DARK_MODE'
 export function getCity(city) {
 
    return async function (dispatch) {
-         dispatch({ type: LOADING, payload: true })
-         return fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&days=5&aqi=no&alerts=no`)
-            .then(r => r.json())
-            .then(r => {
-               dispatch({
-                  type: GET_CITY,
-                  payload: r
-               })
-               dispatch({ type: LOADING, payload: false })
+      dispatch({ type: LOADING, payload: true })
+      return fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&days=5&aqi=no&alerts=no`)
+         .then(r => r.json())
+         .then(r => {
+            dispatch({
+               type: GET_CITY,
+               payload: r
             })
 
-      }
+            dispatch({ type: LOADING, payload: false })
+         })
+         .catch(err => console.log(err))
+
+   }
 
 }
 
@@ -36,17 +38,17 @@ export function deleteSearch(id) {
    }
 }
 
- export function clearDetail(){
-   return{
+export function clearDetail() {
+   return {
       type: CLEAR_DETAIL
    }
-} 
-
-export function darkMode(payload){
-return{
-   type: DARK_MODE,
-   payload
 }
+
+export function darkMode(payload) {
+   return {
+      type: DARK_MODE,
+      payload
+   }
 }
 
 
