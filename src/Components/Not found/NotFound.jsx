264f6cb8de0f71fcb9fa128a86e526410609dae6
NereaMansilla <evalbarreiro@hotmail.com>
2022-09-26 19:38:06 -0300

    fix style notfound

--- src/Components/Not found/NotFound.jsx
+++ src/Components/Not found/NotFound.jsx
@@ -11,11 +11,16 @@ export const NotFound = () => {
   const mode = useSelector(state => state.dark)
   const style = {fontSize:"1em"}
   return (
+    <div>
+      
     <div className={ `${s.containerNotfound} ${mode ? s.lightcontainerNotFound : s.darkcontainerNotFound}` }>
     <img className={s.blaknMap} src={map} alt='blank map' />
-    <h1 className={`${s.textNotFound} ${mode ? s.textNotFoundLight : s.textNotFoundDark}`}>Oh no! we cannot find this city</h1>
-    <h1 className={`${s.text2NotFound} ${mode ? s.text2NotFoundLight : s.text2NotFoundDark}`}>Please try your search again <ImSearch style={style}/></h1>
-
     </div>
+    
+    <h1 className={`${s.textNotFound} ${mode ? s.textNotFoundLight : s.textNotFoundDark}`}>Oh no! we cannot find this city</h1>
+ <h1 className={`${s.text2NotFound} ${mode ? s.text2NotFoundLight : s.text2NotFoundDark}`}>Please try your search again <ImSearch style={style}/></h1>
+
+
+</div>
   )
 }
