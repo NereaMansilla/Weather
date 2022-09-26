20e12f4fbe7db6d635d6bf6742078db9b01d336f
NereaMansilla <evalbarreiro@hotmail.com>
2022-09-26 18:45:21 -0300

    change axios to fetch in actions

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,12 +1,12 @@
-import axios from 'axios'
+
 export const GET_CITY = 'GET_CITY'
 export const GET_DETAILS = 'GET_DETAILS'
 export const DELETE_SEARCH = 'DELETE_SEARCH'
 export const LOADING = 'LOADING'
 export const CLEAR_DETAIL = 'CLEAR_DETAIL'
 export const DARK_MODE = 'DARK_MODE'
-const apiKey = process.env.REACT_APP_WEATHER_KEY
-export function getCity(city) {
+/* const apiKey = process.env.REACT_APP_WEATHER_KEY */
+/* export function getCity(city) {
    return async function (dispatch) {
       try {
          dispatch({ type: LOADING, payload: true })
@@ -21,15 +21,15 @@ export function getCity(city) {
          console.log(error)
       }
    }
-}
+} */
 
 
-/* export function getCity(city) {
+export function getCity(city) {
 
    return async function (dispatch) {
       dispatch({ type: LOADING, payload: true })
-       const ciudad = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=ee7ed3f16f36487e80f62924220607&q=${city}&days=5&aqi=no&alerts=no`)
-         .then(r => r.r.data)
+       fetch(`http://api.weatherapi.com/v1/forecast.json?key=ee7ed3f16f36487e80f62924220607&q=${city}&days=5&aqi=no&alerts=no`)
+         .then(r => r.json())
          .then(r => {
             dispatch({
                type: GET_CITY,
@@ -42,7 +42,7 @@ export function getCity(city) {
 
    }
 
-} */
+}
 
 
 
